5089eb31cf1882b2449344fc4576baea56f7989b
enjesh <enjesh@gmail.com>
2023-11-07 15:58:24 +0300

    configure settings.py to read the .env file

--- config/settings.py
+++ config/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
+from dotenv import load_dotenv
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -19,11 +21,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
+# Load environment variables from .env file
+load_dotenv()
+
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = ('DEBUG')
+DEBUG = os.environ.get('DEBUG') == 'True'  # Convert to a boolean
 
 ALLOWED_HOSTS = []
 INTERNAL_IPS = ["127.0.0.1",]
